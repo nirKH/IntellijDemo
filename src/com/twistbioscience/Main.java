92fa5bd11ab1ff6a1ec400061304274019d2d838
Yaniv Bar Giora <ybgiora@twistbioscience.com>
2019-09-15 17:08:18 +0300

    Add "Hello World2" message

--- src/com/twistbioscience/Main.java
+++ src/com/twistbioscience/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.printIn("Hello World");
+        System.out.printIn("Hello World2");
     }
 }
